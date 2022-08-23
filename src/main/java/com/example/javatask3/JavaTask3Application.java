c7b60fd5da404c764bbd8d904d81e3cfd4072bfc
[HarshSharma811] <[harsh.sharma1_cs19@gla.ac.in]>
2022-08-23 23:19:20 +0530

    Initial commit

---
+++ src/main/java/com/example/javatask3/JavaTask3Application.java
@@ -0,0 +1,13 @@
+package com.example.javatask3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTask3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTask3Application.class, args);
+    }
+
+}
